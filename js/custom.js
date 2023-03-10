449203ce3853f35b77276b47a4e3eacd7cda1b4f
gamestorelife <sales.flinthome@gmail.com>
2023-03-10 16:23:06 +0200

    update on loading

--- js/custom.js
+++ js/custom.js
@@ -31,6 +31,20 @@ document.getElementById("apparts-smenu").style.display = "none";
 
 
 
+document.onreadystatechange = function() {
+	if (document.readyState !== "complete") {
+		document.querySelector(
+		"body").style.visibility = "hidden";
+		document.querySelector(
+		"#loader").style.visibility = "visible";
+	} else {
+		document.querySelector(
+		"#loader").style.display = "none";
+		document.querySelector(
+		"body").style.visibility = "visible";
+	}
+};
+  
 
 
 
