cb41a4527b8e09bcdb7750bcf65659cad7cbe1cc
gamestorelife <sales.flinthome@gmail.com>
2023-03-11 04:38:42 +0200

    update

--- js/one-test.js
+++ js/one-test.js
@@ -18,14 +18,20 @@ villaform.addEventListener('submit', (event) => {
   hotelform.addEventListener('submit', (event) => {
     event.preventDefault(); // prevent the form from submitting
   
-    const allkids = hotelform.elements['hotel-allkids'].value;
-    const allrooms = hotelform.elements['hotel-allrooms'].value;
-    const alladult = hotelform.elements['hotel-alladult'].value;
+    const hallkids = hotelform.elements['hotel-allkids'].value;
+    const hallrooms = hotelform.elements['hotel-allrooms'].value;
+    const halladult = hotelform.elements['hotel-alladult'].value;
+    const hotelkid1 = hotelform.elements['hotel-kid1-age'].value;
+    const hotelkid2 = hotelform.elements['hotel-kid2-age'].value;
+    const hotelkid3 = hotelform.elements['hotel-kid3-age'].value;
+    const hotelkid4 = hotelform.elements['hotel-kid4-age'].value;
+    const hotelkid5 = hotelform.elements['hotel-kid5-age'].value;
+    const hotelkid6 = hotelform.elements['hotel-kid6-age'].value;
   
-    console.log(`allkids: ${allkids}`);
-    console.log(`allrooms: ${allrooms}`);
-    console.log(`alladult: ${alladult}`);
-    window.location.href = "https://bookings.mykonosbooker.com/#/search/2023~2F03~2F11/2023~2F03~2F25/Athens%20Center,%20Athens/OT-LOC-GEO-9186/(nationality:75,rooms:!(('$$hashKey':'object:51',adults:${alladult},children:${allkids},childrenAges:!(),quantity:1)),searchOnRequestRooms:!f,stars:(max:5,min:0))";
+    console.log(`allkids: ${hallkids}`);
+    console.log(`allrooms: ${hallrooms}`);
+    console.log(`alladult: ${halladult}`);
+    window.location.href = `https://bookings.mykonosbooker.com/#/search/2023~2F03~2F11/2023~2F03~2F25/Athens%20Center,%20Athens/OT-LOC-GEO-9186/(nationality:75,rooms:!(('$$hashKey':'object:51',adults:${halladult},children:${hallkids},childrenAges:!(${hotelkid1},${hotelkid2},${hotelkid3},${hotelkid3},${hotelkid4},${hotelkid5},${hotelkid6}),quantity:1)),searchOnRequestRooms:!f,stars:(max:5,min:0))`;
   
 
 });
